72c33d930c620f379394d6680aaeb08deb7846b9
Spencer Everly <spencer.everly@gmail.com>
2023-09-20 10:12:25 -0500

    Update _fix_merge_issues.bat

--- _fix_merge_issues.bat
+++ _fix_merge_issues.bat
@@ -1,6 +1,6 @@
 git remote add upstream https://github.com/Wohlstand/TheXTech.git
 git fetch upstream
-git merge upstream/main
-git pull origin main
+git merge upstream/master
+git pull origin master
 pause
 exit
